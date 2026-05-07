b02f47b3330d0195c260ecec9b2248522c7690a3
malwhile <paul@firefly.lan>
2026-05-06 23:42:55 -0400

    fixing

--- client/src/pages/AdminMembersView.tsx
+++ client/src/pages/AdminMembersView.tsx
@@ -1,4 +1,4 @@
-import React, { useState, ChangeEvent, FormEvent, useEffect } from "react";
+import React, { useState, FormEvent, useEffect } from "react";
 import { useNavigate } from 'react-router';
 import {
   Card,
